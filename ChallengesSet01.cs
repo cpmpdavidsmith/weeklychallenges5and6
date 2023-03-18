d681d6679ccaa88990fcc6bbac3888b6ddb9d3e5
David Smith <cpmpdavidsmith@icloud.com>
2023-03-17 20:38:05 -0400

    5 and 6

---
+++ ChallengesSet01.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet01
+    {
+        //--------------------------------------------------------------------
+        public bool AreTwoNumbersTheSame(int num1, int num2)
+        {
+            if (num1 == num2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //--------------------------------------------------------------------
+        public double Subtract(double minuend, double subtrahend)
+        {
+            double answer = minuend - subtrahend;
+
+            return answer;
+        }
+
+        //--------------------------------------------------------------------
+        public int Add(int number1, int number2)
+        {
+            int answer = number1 + number2;
+
+            return answer;
+        }
+
+        //--------------------------------------------------------------------
+        public int GetSmallestNumber(int number1, int number2)
+        {
+            if (number1 < number2)
+            {
+                return number1;
+            }
+            else
+            {
+                return number2;
+            }
+
+        }
+
+        //--------------------------------------------------------------
+        public long Multiply(long factor1, long factor2)
+        {
+            return factor1 * factor2;
+        }
+
+        //--------------------------------------------------------------
+        public string GetGreeting(string nameOfPerson)
+        {
+
+            if (nameOfPerson == "")
+            {
+                return "Hello!";
+            }
+            else
+            {
+                return $"Hello, {nameOfPerson}!";
+            }
+
+        }
+
+        //--------------------------------------------------------------
+        public string GetHey()
+        {
+            return "HEY!";
+        }
+    }
+}
+
---
+++ ChallengesSet02.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ChallengesWithTestsMark8
+{
+    public class ChallengesSet02
+    {
+        //--------------------------------------------------------------------
+        public bool CharacterIsALetter(char c)
+        {
+            return Char.IsLetter(c);
+        }
+
+        //--------------------------------------------------------------------
+        public bool CountOfElementsIsEven(string[] vals)
+        {
+
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //--------------------------------------------------------------------
+        public bool IsNumberEven(int number)
+        {
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        //--------------------------------------------------------------------
+        public bool IsNumberOdd(int num)
+        {
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            };
+        }
+
+        //--------------------------------------------------------------------
+        public double SumOfMinAndMax(IEnumerable<double> numbers)
+        {
+            if (numbers == null || numbers.Count() == 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return numbers.Min() + numbers.Max();
+            }
+
+        }
+
+        //--------------------------------------------------------------------
+        public int GetLengthOfShortestString(string str1, string str2)
+        {
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
+        }
+
+        //--------------------------------------------------------------------
+        public int Sum(int[] numbers)
+        {
+            if (numbers == null)
+            {
+                return 0;
+            }
+            return numbers.Sum();
+        }
+
+        //--------------------------------------------------------------------
+        public int SumEvens(int[] numbers)
+        {
+            while (numbers != null)
+            {
+                int sum = 0;
+                for (int i = 0; i < numbers.Length; i++)
+                {
+                    if (numbers[i] % 2 == 0)
+                    {
+                        sum += numbers[i];
+                    }
+                }
+                return sum;
+            }
+            return 0;
+        }
+
+        //--------------------------------------------------------------------
+        public bool IsSumOdd(List<int> numbers)
+        {
+            while (numbers != null)
+            {
+                int total = numbers.Sum();
+                if (total % 2 != 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        //--------------------------------------------------------------------
+        public long CountOfPositiveOddsBelowNumber(long number)
+        {
+            long count = 0;
+            for (int i = 1; i < number; i += 2)
+            {
+                count++;
+            }
+            return count;
+        }
+    }
+}
